788515f85e76f95bdcc632c6511b6193ded2aec6
riya-pawar18 <riyapawar941@gmail.com>
2025-05-21 00:30:49 +0530

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Mern
-Web dev files
--- assign3.js
+++
@@ -1,7 +0,0 @@
-const image= document.getElementById("bw");
-image.addEventListener("pointerenter",()=> {
-    image.setAttribute("src","color.jpg");
-});
-image.addEventListener("pointerleave",()=>{
-    image.setAttribute("src","nocolor.jpg");
-});
